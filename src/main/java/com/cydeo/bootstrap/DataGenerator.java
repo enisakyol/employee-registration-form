4da8796d3789b682bdbd548fb6e050a50afb0dcb
enis <enis_akyol@yahoo.com>
2023-09-18 10:26:19 +0300

    employee form with database

---
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,10 @@
+package com.cydeo.bootstrap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+    public static List<String> getAllStates(){
+        return Arrays.asList("Alaska", "Alabama", "New York", "Boston", "Connecticut");
+    }
+}
